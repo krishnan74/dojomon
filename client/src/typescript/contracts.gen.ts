f4c0f607542cc551e531cf4857aa0ff60ea63a5d
krishnan74 <divyakrishnan.r74@gmail.com>
2025-01-13 19:00:18 +0000

    feat - Implement spawnPlayer and createDojomon actions with calldata builders

--- client/src/typescript/contracts.gen.ts
+++ client/src/typescript/contracts.gen.ts
@@ -1,89 +1,111 @@
 import { DojoProvider } from "@dojoengine/core";
-import { Account, AccountInterface, CairoCustomEnum } from "starknet";
+import {
+  Account,
+  AccountInterface,
+  BigNumberish,
+  CairoCustomEnum,
+} from "starknet";
 import * as models from "./models.gen";
 
 export function setupWorld(provider: DojoProvider) {
-    const build_actions_move_calldata = (direction: models.Direction) => {
-
-        const directionToString = {
-      0: 'Left',
-      1: 'Right',
-      2: 'Up',
-      3: 'Down',
-    }
-
-        return {
-            contractName: "actions",
-            entrypoint: "move",
-            calldata: [new CairoCustomEnum({ [directionToString[direction]]: '()' })],
-        };
+  const build_actions_spawn_calldata = (dojomon_type: models.DojomonType) => {
+    const dojomonTypeToString = {
+      0: "Fire",
+      1: "Water",
+      2: "Grass",
     };
-
-    const actions_move = async (
-        snAccount: Account | AccountInterface,
-        direction: models.Direction
-    ) => {
-        try {
-            return await provider.execute(
-                snAccount,
-                build_actions_move_calldata(direction),
-                "dojo_starter"
-            );
-        } catch (error) {
-            console.error(error);
-            throw error;
-        }
-    };
-
-    const build_actions_spawn_calldata = () => {
-        return {
-            contractName: "actions",
-            entrypoint: "spawn",
-            calldata: [],
-        };
-    };
-
-    const actions_spawn = async (snAccount: Account | AccountInterface) => {
-        try {
-            return await provider.execute(
-                snAccount,
-                build_actions_spawn_calldata(),
-                "dojo_starter"
-            );
-        } catch (error) {
-            console.error(error);
-            throw error;
-        }
-    };
-
-    const build_actions_upgradeSpeed_calldata = () => {
-        return {
-            contractName: "actions",
-            entrypoint: "upgradeSpeed",
-            calldata: [],
-        };
-    }
-
-    const actions_upgradeSpeed = async (snAccount: Account | AccountInterface) => {
-        try {
-            return await provider.execute(
-                snAccount,
-                build_actions_upgradeSpeed_calldata(),
-                "dojo_starter"
-            );
-        } catch (error) {
-            console.error(error);
-            throw error;
-        }
-    }
 
     return {
-        actions: {
-            move: actions_move,
-            buildMoveCalldata: build_actions_move_calldata,
-            spawn: actions_spawn,
-            buildSpawnCalldata: build_actions_spawn_calldata,
-            upgradeSpeed: actions_upgradeSpeed,
-        },
+      contractName: "actions",
+      entrypoint: "spawnPlayer",
+      calldata: [
+        new CairoCustomEnum({ [dojomonTypeToString[dojomon_type]]: "()" }),
+      ],
     };
+  };
+
+  const actions_spawn = async (
+    snAccount: Account | AccountInterface,
+    dojoball_type: models.DojomonType
+  ) => {
+    try {
+      return await provider.execute(
+        snAccount,
+        build_actions_spawn_calldata(dojoball_type),
+        "dojo_starter"
+      );
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
+
+  const build_actions_createDojomon_calldata = (
+    name: string,
+    health: BigNumberish,
+    attack: BigNumberish,
+    defense: BigNumberish,
+    speed: BigNumberish,
+    dojomon_type: models.DojomonType,
+    position: models.Position
+  ) => {
+    const dojomonTypeToString = {
+      0: "Fire",
+      1: "Water",
+      2: "Grass",
+    };
+
+    return {
+      contractName: "actions",
+      entrypoint: "createDojomon",
+      calldata: [
+        name,
+        health,
+        attack,
+        defense,
+        speed,
+        new CairoCustomEnum({ [dojomonTypeToString[dojomon_type]]: "()" }),
+        position,
+      ],
+    };
+  };
+
+  const actions_createDojomon = async (
+    snAccount: Account | AccountInterface,
+    name: string,
+    health: BigNumberish,
+    attack: BigNumberish,
+    defense: BigNumberish,
+    speed: BigNumberish,
+    dojomon_type: models.DojomonType,
+    position: models.Position
+  ) => {
+    try {
+      return await provider.execute(
+        snAccount,
+        build_actions_createDojomon_calldata(
+          name,
+          health,
+          attack,
+          defense,
+          speed,
+          dojomon_type,
+          position
+        ),
+        "dojo_starter"
+      );
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
+
+  return {
+    actions: {
+      spawnPlayer: actions_spawn,
+      buildSpawnCalldata: build_actions_spawn_calldata,
+      createDojomon: actions_createDojomon,
+      buildCreateDojomonCalldata: build_actions_createDojomon_calldata,
+    },
+  };
 }
