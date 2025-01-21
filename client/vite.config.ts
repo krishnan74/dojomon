1cfb4f614f0b52e5cb647873437cac41a442c677
krishnan74 <divyakrishnan.r74@gmail.com>
2025-01-21 05:30:16 +0000

    one more

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,6 +7,7 @@ import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react(), wasm(), topLevelAwait(), mkcert()],
   resolve: {
     alias: {
